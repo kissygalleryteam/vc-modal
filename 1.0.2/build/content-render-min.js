4688d73728a47afd20a58e846b1a4aaad9a0607b
kissyteam <tiehang.lth@taobao.com>
2015-01-22 19:36:27 +0800

    sync daily version 1.0.2

---
+++ 1.0.2/build/content-render-min.js
@@ -0,0 +1,1 @@
+KISSY.add("kg/vc-modal/1.0.2/content-render",["./content","kg/xtemplate/3.3.3/runtime"],function(e,n,t,r){var a=n("./content"),o=n("kg/xtemplate/3.3.3/runtime"),c=new o(a);r.exports=function(){return c.render.apply(c,arguments)}});
---
+++ 1.0.2/build/deps-min.js
@@ -0,0 +1,1 @@
+KISSY.config("requires",{"kg/vc-modal/1.0.2/content-render":["./content","kg/xtemplate/3.3.3/runtime"],"kg/vc-modal/1.0.2/index":["./index.css","base","node","event","promise","ua","kg/xtemplate/3.3.3/runtime","./content"],"kg/vc-modal/1.0.2/lib/main":["node","base"]});
